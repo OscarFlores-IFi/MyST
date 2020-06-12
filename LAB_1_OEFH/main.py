2a74c005c77f5a6b781bbfab541b4704271181cd
OscarFlores-IFi <if715029@gmail.com>
2020-06-11 19:00:38 -0500

    cruces media movil

--- LAB_1_OEFH/main.py
+++ LAB_1_OEFH/main.py
@@ -4,6 +4,7 @@ import download as fn                              # Para procesamiento de datos
 import pandas as pd                                 # Procesamiento de datos
 import numpy as np
 import plotly.express as px
+from itertools import combinations as cmb
 
 # -- --------------------------------------------------------- Descargar precios de OANDA -- #
 
@@ -11,8 +12,8 @@ import plotly.express as px
 OA_Ak = '107596e9d65c' + '1bbc9175953d917140' + '12-f975c6201dddad03ac1592232c0ea0ea'
 Id = ["EUR_USD", "AUD_USD", "GBP_USD", "USD_JPY", "EUR_JPY"]                  # Instrumento
 OA_Gn = "H1"                        # Granularidad de velas
-fini = pd.to_datetime("2019-07-06 00:00:00").tz_localize('GMT')  # Fecha inicial
-ffin = pd.to_datetime("2019-12-06 00:00:00").tz_localize('GMT')  # Fecha final
+fini = pd.to_datetime("2018-07-01 00:00:00").tz_localize('GMT')  # Fecha inicial
+ffin = pd.to_datetime("2018-12-31 00:00:00").tz_localize('GMT')  # Fecha final
 
 # Descargar precios masivos
 
@@ -26,13 +27,20 @@ closes = pd.DataFrame({Id[i]:data[Id[i]].Close for i in range(len(Id))}).dropna(
 (closes/closes.iloc[0,:]).plot()
 
 # Creamos la matriz de Promedios móviles para cada activo
-
 vent = [3, 5, 8, 13, 21, 34]
 MeanAvg = {j:pd.DataFrame({i:closes[j].rolling(i).mean() for i in vent}).dropna() for j in Id}
 
+# Calculamos las combinaciones de activos posibles para cada activo.
+comb = [i for i in cmb(vent,2)]
+t = len(MeanAvg[Id[0]])
 
-
-
+# Calculamos cuando la media movil rápida cruza la lenta.
+tmp = np.zeros((t,len(comb)))
+Cruces = {}
+for j in Id:
+    for i in range(len(comb)):
+        tmp[:,i] = MeanAvg[Id[0]][comb[i][0]]>MeanAvg[Id[0]][comb[i][1]]
+    Cruces[j] = pd.DataFrame(tmp,columns=comb) # Se pierde el índice original.
 
 
 
